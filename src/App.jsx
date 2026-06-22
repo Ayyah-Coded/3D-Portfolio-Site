9b85c44dfb940b31e32bada16fd00d0d2264056a
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-22 15:13:19 +0100

    - Implement the 'Home' section, spiced it up with motion and react-three/fiber libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Contact from "./components/contact/Contact"
+import Hero from "./components/hero/hero"
+import Portfolio from "./components/portfolio/Portfolio"
+import Services from "./components/services/Services"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='container'>
+      <section id="#hero"><Hero /></section>
+      <section id="#services"><Services /></section>
+      <section id="#portfolio"><Portfolio /></section>
+      <section id="#contact"><Contact /></section>
+    </div>
   )
 }
 
