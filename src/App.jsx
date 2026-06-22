ab7c83ebd2817e65ce41b8dc93ae06200ef65e37
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-23 00:37:02 +0100

    - Implement Contact section - Implement lazyload for fast rendering of contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import Contact from "./components/contact/Contact"
-import Hero from "./components/hero/hero"
-import Portfolio from "./components/portfolio/Portfolio"
-import Services from "./components/services/Services"
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+
+const Contact = lazy(() => import("./components/contact/Contact"))
+const Hero = lazy(() => import("./components/hero/Hero"))
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"))
+const Services = lazy(() => import("./components/services/Services"))
 
 const App = () => {
   return (
-    <div className='container'>
-      <section id="#hero"><Hero /></section>
-      <section id="#services"><Services /></section>
-      <section id="#portfolio"><Portfolio /></section>
-      <section id="#contact"><Contact /></section>
+    <div className="container">
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#home">
+            <Hero />
+          </section>
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#services">
+            <Services />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"600vh"} offset={-100}>
+          {/* <section id="#portfolio"> */}
+          <Portfolio />
+          {/* </section> */}{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#contact">
+            <Contact />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
